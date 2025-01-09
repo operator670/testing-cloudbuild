1c2e6c6d225d599ef26506ccedf50449bd778e00
Sarthak Kesarwani <57749538+operator670@users.noreply.github.com>
2025-01-09 14:37:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// You can add simple Javascript here if needed
+// For example:
+
+const messageElement = document.getElementById('message');
+
+if (messageElement) {
+   messageElement.textContent = "This text is modified by javascript";
+}
